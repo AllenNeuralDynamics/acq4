6a33cca3f4fd0b4fb9ec0e117c4532bf95f778a8
svc_mpe <svc_mpe@alleninstitute.org>
2020-02-06 09:35:10 -0800

    updated calibration and stage thresholds

--- acq4/devices/Scientifica/scientifica.py
+++ acq4/devices/Scientifica/scientifica.py
@@ -277,7 +277,7 @@ class ScientificaMoveFuture(MoveFuture):
         # did we reach target?
         pos = self.dev._getPosition()
         dif = ((np.array(pos) - np.array(self.targetPos))**2).sum()**0.5
-        if dif < 2.5e-6:
+        if dif < 2.6e-6:
             # reached target
             self._finished = True
             return 1
